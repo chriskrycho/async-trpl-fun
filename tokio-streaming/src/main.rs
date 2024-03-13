a6981e61d46960a3af7e372a71f617f09985d3b7
Chris Krycho <hello@chriskrycho.com>
2024-03-13 10:51:30 -0600

    Add tokio streaming example
    This also gave some extra insight into the double-`.await` hazard,
    especially combined with @skade’s comment on Twitter about the design
    of the `Poll` type today, so I added that to the notes.

---
+++ tokio-streaming/src/main.rs
@@ -0,0 +1,45 @@
+use mini_redis::client;
+use tokio_stream::StreamExt;
+
+#[tokio::main]
+async fn main() -> mini_redis::Result<()> {
+    tokio::spawn(async { publish().await });
+
+    subscribe().await?;
+
+    println!("DONE");
+
+    Ok(())
+}
+
+async fn subscribe() -> mini_redis::Result<()> {
+    let client = client::connect("127.0.0.1:6379").await?;
+    let subscriber = client.subscribe(vec!["numbers".to_string()]).await?;
+    let messages = subscriber.into_stream();
+
+    tokio::pin!(messages);
+
+    while let Some(msg) = messages.next().await {
+        println!("got = {msg:?}");
+    }
+
+    // When the server closes, this will stop getting messages, and specifically
+    // the `Future` will end so the above stream will end, by way of returning
+    // `Poll::Ready(None)` via the implicit polling which happens in `.await`.
+    // This `println!` is a handy little signal of just that in this demo.
+    println!("(closed)");
+
+    Ok(())
+}
+
+async fn publish() -> mini_redis::Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    client.publish("numbers", "1".into()).await?;
+    client.publish("numbers", "two".into()).await?;
+    client.publish("numbers", "3".into()).await?;
+    client.publish("numbers", "four".into()).await?;
+    client.publish("numbers", "five".into()).await?;
+    client.publish("numbers", "6".into()).await?;
+    Ok(())
+}
