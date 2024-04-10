b58345407c41dc09568dc2cb19ba563b613ac313
Chris Krycho <hello@chriskrycho.com>
2024-04-10 15:36:22 -0600

    lr2: further (and fairly dramatically!) simplify watcher
    I was, again, just plain overthinking this. There is no need to spawn a
    task here at all: it is already in an `async fn`, so it will be run in a
    task automatically by its *parent*. Additionally, pulling the call to
    `watcher.watch()` out into the `watch` makes this much easier to follow.

--- lr2/src/lib.rs
+++ lr2/src/lib.rs
@@ -1,55 +1,18 @@
 use std::path::Path;
 
-use notify::{Config, Event, RecommendedWatcher, RecursiveMode, Watcher};
-use tokio::{sync::mpsc, task::JoinSet};
+use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
+use tokio::{sync::mpsc, task};
 
 pub async fn watch(dir: &Path) -> Result<(), Error> {
-    let (events_tx, mut events_rx) = mpsc::channel(1024);
-    let (errors_tx, mut errors_rx) = mpsc::channel(1024);
+    let (tx, mut rx) = mpsc::channel(8);
 
-    // This needs to be here simply so that the watcher continues to exist until
-    // the rest of the operations stop yielding values.
-    let _watcher = watch_notify(dir, events_tx.clone(), errors_tx.clone())?;
-
-    let mut set = JoinSet::new();
-    set.spawn(async move {
-        while let Some(evt) = events_rx.recv().await {
-            println!("Got event! {:?}", evt.kind);
-        }
-    });
-
-    set.spawn(async move {
-        while let Some(err) = errors_rx.recv().await {
-            eprintln!("Got error! {err}");
-        }
-    });
-
-    while let Some(result) = set.join_next().await {
-        match result {
-            Ok(_) => println!("Succeeded."),
-            Err(reason) => eprintln!("Failed: {reason}"),
-        }
-    }
-
-    Ok(())
-}
-
-fn watch_notify(
-    dir: &Path,
-    events: mpsc::Sender<Event>,
-    errors: mpsc::Sender<Error>,
-) -> Result<RecommendedWatcher, Error> {
+    // Doing this here means we will not drop the watcher until this function
+    // ends, and the `while let` below will continue until there is an error (or
+    // something else shuts down the whole system here!).
     let mut watcher = RecommendedWatcher::new(
-        move |watch_result| match watch_result {
-            Ok(event) => {
-                if let Err(e) = events.try_send(event) {
-                    eprintln!("Could not handle error gracefully.\nError:{e}");
-                }
-            }
-            Err(source) => {
-                if let Err(e) = errors.try_send(Error::Notify { source }) {
-                    eprintln!("Could not handle error gracefully.\nError:{e}");
-                }
+        move |result| {
+            if let Err(e) = tx.try_send(result) {
+                eprintln!("Could not handle error gracefully.\nError:{e}");
             }
         },
         Config::default(),
@@ -60,7 +23,16 @@ fn watch_notify(
         .watch(dir, RecursiveMode::Recursive)
         .map_err(Error::from)?;
 
-    Ok(watcher)
+    while let Some(result) = rx.recv().await {
+        match result {
+            Ok(event) => {
+                println!("Got event! {:?}", event.kind);
+            }
+            Err(reason) => return Err(Error::from(reason)),
+        }
+    }
+
+    Ok(())
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -69,5 +41,11 @@ pub enum Error {
     Notify {
         #[from]
         source: notify::Error,
+    },
+
+    #[error(transparent)]
+    Tokio {
+        #[from]
+        source: task::JoinError,
     },
 }
