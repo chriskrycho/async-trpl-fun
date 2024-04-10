34497f858a07949817b89df877e704bb62f7f7a8
Chris Krycho <hello@chriskrycho.com>
2024-04-10 16:04:41 -0600

    lr2: use notify-debouncer-full to debounce changes
    This is not as robust as it needs to be: both fsevents and kqueue end up
    delivering *way* more events than I would expect, and the debouncing
    does not actually handle all of them, even at fairly large intervals.

--- lr2/src/lib.rs
+++ lr2/src/lib.rs
@@ -1,38 +1,57 @@
-use std::path::Path;
+use std::{path::PathBuf, time::Duration};
 
-use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
-use tokio::{sync::mpsc, task};
+use notify_debouncer_full::{
+    new_debouncer,
+    notify::{RecursiveMode, Watcher},
+    DebouncedEvent,
+};
+use tokio::{
+    sync::mpsc::{self, Sender},
+    task,
+};
 
-pub async fn watch(dir: &Path) -> Result<(), Error> {
-    let (tx, mut rx) = mpsc::channel(8);
+pub async fn watch(
+    dir: PathBuf,
+    event_channel: Sender<Change>,
+    timeout: Duration,
+) -> Result<(), Error> {
+    let (tx, mut rx) = mpsc::channel(256);
 
     // Doing this here means we will not drop the watcher until this function
     // ends, and the `while let` below will continue until there is an error (or
     // something else shuts down the whole system here!).
-    let mut watcher = RecommendedWatcher::new(
-        move |result| {
-            if let Err(e) = tx.try_send(result) {
-                eprintln!("Could not handle error gracefully.\nError:{e}");
-            }
-        },
-        Config::default(),
-    )
+    let mut debounced = new_debouncer(timeout, None, move |result| {
+        if let Err(e) = tx.blocking_send(result) {
+            eprintln!("Could not send event.\nError:{e}");
+        }
+    })
     .map_err(Error::from)?;
 
-    watcher
-        .watch(dir, RecursiveMode::Recursive)
+    debounced
+        .watcher()
+        .watch(&dir, RecursiveMode::Recursive)
         .map_err(Error::from)?;
 
     while let Some(result) = rx.recv().await {
         match result {
-            Ok(event) => {
-                println!("Got event! {:?}", event.kind);
+            Ok(debounced_events) => {
+                for DebouncedEvent { event, .. } in debounced_events {
+                    let change = Change { paths: event.paths };
+                    if let Err(e) = event_channel.send(change).await {
+                        eprintln!("Error sending out: {e:?}");
+                    }
+                }
             }
-            Err(reason) => return Err(Error::from(reason)),
+            Err(reasons) => return Err(Error::from(reasons)),
         }
     }
 
     Ok(())
+}
+
+#[derive(Debug)]
+pub struct Change {
+    pub paths: Vec<PathBuf>,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -43,9 +62,37 @@ pub enum Error {
         source: notify::Error,
     },
 
+    #[error("{0}")]
+    NotifyPlural(NotifyReasons),
+
     #[error(transparent)]
     Tokio {
         #[from]
         source: task::JoinError,
     },
+}
+
+impl From<Vec<notify::Error>> for Error {
+    fn from(value: Vec<notify::Error>) -> Self {
+        Error::NotifyPlural(NotifyReasons { sources: value })
+    }
+}
+
+#[derive(Debug)]
+pub struct NotifyReasons {
+    sources: Vec<notify::Error>,
+}
+
+impl std::fmt::Display for NotifyReasons {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.sources
+                .iter()
+                .map(|source| format!("{source:?}"))
+                .collect::<Vec<_>>()
+                .join("\n")
+        )
+    }
 }
--- lr2/src/main.rs
+++ lr2/src/main.rs
@@ -1,10 +1,41 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, time::Duration};
 
 use lr2::watch;
 
 #[tokio::main]
-async fn main() -> Result<(), lr2::Error> {
-    let dir = std::env::args().nth(1).unwrap();
+async fn main() -> Result<(), String> {
+    let dir = std::env::args().nth(1).map(PathBuf::from).unwrap();
     let dir = PathBuf::from(dir);
-    watch(&dir).await
+    let (tx, mut rx) = tokio::sync::mpsc::channel(256);
+
+    let mut set = tokio::task::JoinSet::new();
+
+    set.spawn(watch(dir, tx, Duration::from_secs(1)));
+    set.spawn(async move {
+        while let Some(lr2::Change { paths }) = rx.recv().await {
+            println!(
+                "Got a change! Paths: {}",
+                paths
+                    .iter()
+                    .map(|path| format!("{}", path.display()))
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            );
+        }
+
+        Ok(())
+    });
+
+    while let Some(result) = set.join_next().await {
+        match result {
+            Ok(inner) => {
+                if let Err(reason) = inner {
+                    return Err(format!("{reason}"));
+                }
+            }
+            Err(reason) => return Err(format!("{reason}")),
+        }
+    }
+
+    Ok(())
 }
